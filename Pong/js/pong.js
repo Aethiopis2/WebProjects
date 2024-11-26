7a5d2e1309c639bed82af7de8fe0065f3bd73cd9
Aethiopis2 <PanaceaSolutionsEth@gmail.com>
2024-11-27 00:40:26 +0300

    mouse inputs

--- Pong/js/pong.js
+++ Pong/js/pong.js
@@ -4,48 +4,92 @@
  * @date 25th of November 2024, Monday
  */
 window.onload = () => {
-    const header = document.getElementById('header');
-    const footer = document.getElementById('footer');
-    const game = document.getElementById('game');
-    
     // compute the dimensions of the game area at start-up
-    game.style.height = (window.innerHeight - (header.offsetHeight + footer.offsetHeight)).toString() + "px";
-    
+    dom('game').style.height = (window.innerHeight - (header.offsetHeight + footer.offsetHeight)).toString() + "px";
+
     var pong = {
         paddleA: {
-            x: document.getElementById('paddleA').offsetLeft,
-            y: Math.floor(Math.random() * game.offsetHeight), 
-            width: document.getElementById('paddleA').offsetWidth,
-            height: document.getElementById('paddleA').offsetHeight
+            x: dom('paddleA').offsetLeft,
+            y: Math.floor(Math.random() * (game.offsetHeight - dom('paddleA').offsetHeight)), 
+            width: dom('paddleA').offsetWidth,
+            height: dom('paddleB').offsetHeight
         },
         paddleB: {
-            x: document.getElementById('paddleB').offsetLeft,
-            y: Math.floor(Math.random() * game.offsetHeight), 
-            width: document.getElementById('paddleB').offsetWidth,
-            height: document.getElementById('paddleB').offsetHeight
-        }
+            x: dom('paddleB').offsetLeft,
+            y: Math.floor(Math.random() * (game.offsetHeight - dom('paddleB').offsetHeight)), 
+            width: dom('paddleB').offsetWidth,
+            height: dom('paddleB').offsetHeight
+        },
+        playground: {
+            offsetTop: dom('game').offsetTop,
+        },
     }; 
 
-
-    function renderPaddles() {
-        var paddleA, paddleB;
-        if (!paddleA)
-            paddleA = document.getElementById('paddleA');
-        if (!paddleB)
-            paddleB = document.getElementById('paddleB');
-        
-        console.log(pong.paddleA.y, pong.paddleA.x, pong.paddleA.width, pong.paddleA.height);
-        paddleA.style.top = pong.paddleA.y;
-        paddleB.style.top = pong.paddleB.y;
+    /**
+     * render's the two dom elements paddleA and paddleB as game object, square
+     *  paddles that go on about up and down 
+     */
+    function renderPaddles() {        
+        dom('paddleA').style.top = pong.paddleA.y + "px";
+        dom('paddleB').style.top = pong.paddleB.y + "px";
     } // end renderPaddles
 
-    renderPaddles();
-}
+    /**
+     * handles the mousemove, mouseenter, and mouseleave events
+     */
+    function handleMouse() {
+        dom('game').addEventListener('mouseenter', () => pong.isPaused = false);
+        dom('game').addEventListener('mouseleave', () => pong.isPaused = true);
+        dom('game').addEventListener('mousemove', e => {
+            pong.paddleA.y = e.pageY - pong.playground.offsetTop;
+            if (pong.paddleA.y + pong.paddleA.height > dom('game').offsetHeight)
+                 pong.paddleA.y = dom('game').offsetHeight - pong.paddleA.height;
+        });
+    } // end handleMouse
+
+    /**
+     * runs at times set by browser to obtain smooth animation
+     */
+    function render() {
+        renderPaddles();
+
+        window.requestAnimationFrame(render);
+    } // end render
+
+    /**
+     * initalizes the game
+     */
+    function init() {
+        // add an event-listener for whenever browser-window size changes
+        window.addEventListener('resize', () => {
+            // recompute the game size 
+            dom('game').style.height = (window.innerHeight - 
+                (dom('header').offsetHeight + dom('footer').offsetHeight)).toString() + "px";
+            pong.paddleA.y = (pong.paddleA.y + pong.paddleA.height >= dom('game').offsetHeight ? 
+                dom('game').offsetHeight - pong.paddleA.height : pong.paddleA.y);
+            pong.paddleB.y = (pong.paddleB.y + pong.paddleB.height >= dom('game').offsetHeight ? 
+                dom('game').offsetHeight - pong.paddleB.height : pong.paddleB.y);
+
+            renderPaddles();
+        });
+
+        window.requestAnimationFrame(render);
+        handleMouse();
+    } // end init
+    
+    init();
+};
 
 
-window.addEventListener('resize', ()=> {
-    const header = document.getElementById('header');
-    const footer = document.getElementById('footer');
-    const game = document.getElementById('game');
-    game.style.height = (window.innerHeight - (header.offsetHeight + footer.offsetHeight)).toString() + "px";
-}, true);
+/**
+ * 
+ * @param {*} elemid the dom element id
+ * @returns the dom element for the supplied id
+ */
+function dom(elemid) {
+    if (!dom.cache) dom.cache = {};
+    if (dom.cache[elemid] != null) return dom.cache[elemid];
+
+    dom.cache[elemid] = document.getElementById(elemid);
+    return dom.cache[elemid];
+} // end memoize dom
